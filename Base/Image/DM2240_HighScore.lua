fc4bb02f4191c60ab5d5aa4ec214912dd452f96b
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-02-09 12:54:10 +0800

    Zip Submission

--- Base/Image/DM2240_HighScore.lua
+++ Base/Image/DM2240_HighScore.lua
@@ -1,8 +1,8 @@
-CPlayerInfoStartPos = {x=3.456548,y=0.000000,z=34.485771}
-CPlayerInfoTargetPos = {x=3.504316,y=0.326736,z=35.429680}
+CPlayerInfoStartPos = {x=5.672800,y=0.000000,z=-153.045685}
+CPlayerInfoTargetPos = {x=4.908941,y=0.197199,z=-152.431168}
 CPlayerInfoLife = 10
-CPistolMagRounds = 1
+CPistolMagRounds = 7
 CPistolMaxMagRounds = 8
 CPistolTotalRounds = 40
 CPistolMaxTotalRounds = 40
-CPistolShotCooldown = 0
+CPistolShotCooldown = -2147483648
